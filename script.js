b0272485a23ceaabcd806acecab12170af003115
Saymiki <mikaela.montesclaros@gmail.com>
2023-06-06 18:00:41 -0500

    added var and characters

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+var enterLimit = "";
+var confirmLowercase;
+var confirmUppercase;
+var confirmSpecial;
+var confirmNumeric;
+
+// creating characters to choose to create password from
+lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+special = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~"];
+numeric = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+
+// creating a function to set parameters for password
+function generatePassword 
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
