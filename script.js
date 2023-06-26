9c843baa6b4fbf96c36b58a1e2449b6cbd8dcc1a
Saymiki <mikaela.montesclaros@gmail.com>
2023-06-26 15:11:40 -0500

    added random and floor method

--- script.js
+++ script.js
@@ -8,14 +8,122 @@ var confirmSpecial;
 var confirmNumeric;
 
 // creating characters to choose to create password from
-lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-special = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~"];
-numeric = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+const randomLowercase = () => {
+  lowercase = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+  ];
+  return lowercase[Math.floor(Math.random() * lowercase.length)];
+};
 
+const randomUppercase = () => {
+  uppercase = [
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J",
+    "K",
+    "L",
+    "M",
+    "N",
+    "O",
+    "P",
+    "Q",
+    "R",
+    "S",
+    "T",
+    "U",
+    "V",
+    "W",
+    "X",
+    "Y",
+    "Z",
+  ];
+  return uppercase[Math.floor(Math.random() * uppercase.length)];
+};
+
+const randomSpecial = () => {
+  special = [
+    "!",
+    "#",
+    "$",
+    "%",
+    "&",
+    "'",
+    "(",
+    ")",
+    "*",
+    "+",
+    ",",
+    "-",
+    ".",
+    "/",
+    ":",
+    ";",
+    "<",
+    "=",
+    ">",
+    "?",
+    "@",
+    "[",
+    "]",
+    "^",
+    "_",
+    "`",
+    "{",
+    "|",
+    "}",
+    "~",
+  ];
+  return symbol[Math.floor(Math.random() * symbol.length)];
+};
+
+const randomNumber = () => {
+  numeric = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+  return num[Math.floor(Math.random() * num.length)];
+};
 
 // creating a function to set parameters for password
-function generatePassword 
+function generatePassword() {
+  // how many characters 8-128
+  var setLimit = parseFloat(
+    prompt("How many characters between 8-128 would you like to use?")
+  );
+  // numbers y/n
+  // uppercase y/n
+  // lowercase y/n
+  // special characters y/n
+}
 
 // Write password to the #password input
 function writePassword() {
@@ -23,7 +131,6 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
